356498482e20ae9a11e618925af558fac5ed172d
lmnhhoang <86773354+lmnhhoang@users.noreply.github.com>
2024-03-04 21:17:05 +0700

    Create base required file for FE source

---
+++ Fontend_QuickFood-main/Screen/SplashScreen.js
@@ -0,0 +1,44 @@
+import {Image, StyleSheet, View} from 'react-native';
+import React, {useEffect, useState,} from 'react';
+
+const SplashScreen = ({navigation}) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      handleLogin();
+    }
+  }, [isLoading, navigation]);
+
+  const handleLogin = async () => {
+    navigation.replace('Appnavigator');
+  };
+
+  return (
+      <View style={styles.container}>
+        <Image source={require('../Image/logo1.png')} style={styles.logo}/>
+      </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  logo: {
+    alignSelf: 'center',
+  },
+});
