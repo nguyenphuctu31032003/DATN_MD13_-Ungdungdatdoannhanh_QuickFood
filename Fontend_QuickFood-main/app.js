85f75dd84c9319e2be9408fa4cb7204b004171ab
cuongmcph27903 <94911808+cuongmc2407@users.noreply.github.com>
2024-03-05 21:07:54 +0700

    update app.js

--- Fontend_QuickFood-main/app.js
+++ Fontend_QuickFood-main/app.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Provider} from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
 import SplashScreen from './Screen/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <Provider>
-        <NavigationContainer>
+    <Provider>
+      <NavigationContainer>
 
-            <Stack.Navigator initialRouteName="SplashScreen"
-                             screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName="SplashScreen"
+          screenOptions={{ headerShown: false }}>
 
-
-            </Stack.Navigator>
-        </NavigationContainer>
-      </Provider>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ title: 'HELLO' }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen}
+            options={{ title: 'Đăng ký' }}
+          />
+          <Stack.Screen name="PayScreen" component={PayScreen} options={{ title: 'Thanh toán' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
